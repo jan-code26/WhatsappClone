9709db67d8d600bc6ee46c954a082960b1b6373a
jahnavi <jahnavipatel0206@gmail.com>
2022-05-14 00:53:31 +0530

    LoginSignupUI

---
+++ app/src/main/java/com/example/whatsappclone/LogInSignUp/PhnoLogInActivity.java
@@ -0,0 +1,16 @@
+package com.example.whatsappclone.LogInSignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.whatsappclone.R;
+
+public class PhnoLogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phno_log_in);
+    }
+}
