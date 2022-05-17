fd618d4d42c416ef2323ad8189c7e26433c8d61a
jahnavi <jahnavipatel0206@gmail.com>
2022-05-17 22:00:12 +0530

    LoginSignup

--- app/src/main/java/com/example/whatsappclone/LogInSignUp/LogInActivity.java
+++ app/src/main/java/com/example/whatsappclone/LogInSignUp/LogInActivity.java
@@ -1,15 +1,53 @@
 package com.example.whatsappclone.LogInSignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.whatsappclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class LogInActivity extends AppCompatActivity {
+
+    TextView needanewAccount,forgotpassword;
+    Button phonenumberlogIn,LogIn;
+    EditText email,password;
+    DatabaseReference UserRef;
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        Intialize();
+        needanewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Sendusertoregactivity();
+            }
+        });
+    }
+
+    private void Sendusertoregactivity() {
+        Intent intent = new Intent(LogInActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private void Intialize() {
+        needanewAccount=findViewById(R.id.needanewAccount);
+        phonenumberlogIn=findViewById(R.id.phone_number_login);
+        email=findViewById(R.id.login_email);
+        password=findViewById(R.id.login_password);
+        LogIn=findViewById(R.id.login_btn);
+        forgotpassword=findViewById(R.id.forgot_password);
+        auth=FirebaseAuth.getInstance();
     }
 }
--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -4,22 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.whatsappclone.LogInSignUp.LogInActivity;
 import com.example.whatsappclone.helper.TabAccesorAdapter;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     ViewPager mainViewPager;
     TabLayout tabLayout;
     TabAccesorAdapter tabAccesorAdapter;
-
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
+    DatabaseReference RootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        RootRef = FirebaseDatabase.getInstance().getReference();
         toolbar = findViewById(R.id.main_activity_toolbar);
         mainViewPager = findViewById(R.id.main_tab_viewPager);
         tabLayout = findViewById(R.id.main_tabs);
@@ -28,5 +40,23 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         tabLayout.setupWithViewPager(mainViewPager);
         getSupportActionBar().setTitle("Whatsapp");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser=auth.getCurrentUser();
+        if (currentUser==null)
+        {
+
+            SendUserToLogInActivity();
+
+        }
+
+    }
+
+    private void SendUserToLogInActivity() {
+        Intent intent =new Intent(MainActivity.this, LogInActivity.class);
+        startActivity(intent);
     }
 }
